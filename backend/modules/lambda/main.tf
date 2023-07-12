96241267633de011a0faab7c267dea6c369d24c1
gustavo bastidas <gustavo_bastidas@mckinsey.com>
2023-07-11 20:27:50 -0600

    fix go build

--- backend/modules/lambda/main.tf
+++ backend/modules/lambda/main.tf
@@ -1,8 +1,3 @@
-resource "aws_cloudwatch_log_group" "for_lambda" {
-  name              = "/aws/lambda/${local.function_name}"
-  retention_in_days = var.cloudwatch_log_retention_in_days
-}
-
 resource "aws_iam_policy" "for_logs_access" {
   name        = format("%s-%s-%s-%s", var.environment, var.solution_name, var.function_name, "for-log-access")
   description = "role policy for ${var.function_name} lambda for cloudwatch logs access"
@@ -15,16 +10,17 @@ resource "aws_iam_role_policy_attachment" "for_logs_access" {
 }
 
 locals {
-  binary_name   = "app"
-  src_path      = "${var.source_path}/main.go"
-  bin_path      = "${var.source_path}/bin/${local.binary_name}"
-  archive_path  = "${var.source_path}/archive/${local.binary_name}.${var.archive_type}"
-  function_name = format("%s-%s-%s", var.environment, var.solution_name, var.function_name)
+  binary_name    = "app"
+  src_file       = "main.go"
+  bin_folder     = "bin"
+  archive_folder = "archive"
+  bin_path       = format("%s/%s/%s", var.source_path, local.bin_folder, local.binary_name)
+  archive_path   = format("%s/%s/%s.%s", var.source_path, local.archive_folder, local.binary_name, var.archive_type)
 }
 
 resource "null_resource" "go_build" {
   provisioner "local-exec" {
-    command = "GOOS=linux GOARCH=amd64 CGO_ENABLED=0 GOFLAGS=-trimpath go build -mod=readonly -ldflags='-s -w' -o ${local.bin_path} ${local.src_path}"
+    command = "cd ${var.source_path} && GOOS=linux GOARCH=amd64 CGO_ENABLED=0 GOFLAGS=-trimpath go build -mod=readonly -ldflags='-s -w' -o ${local.bin_folder}/${local.binary_name} ${local.src_file}"
   }
 }
 
@@ -37,7 +33,7 @@ data "archive_file" "zip" {
 }
 
 resource "aws_lambda_function" "function" {
-  function_name    = local.function_name
+  function_name    = format("%s-%s-%s", var.environment, var.solution_name, var.function_name)
   description      = "${var.function_name} lambda"
   role             = data.aws_iam_role.role.arn
   handler          = local.binary_name
@@ -46,4 +42,9 @@ resource "aws_lambda_function" "function" {
   timeout          = var.timeout
   filename         = data.archive_file.zip.output_path
   source_code_hash = data.archive_file.zip.output_base64sha256
+}
+
+resource "aws_cloudwatch_log_group" "for_lambda" {
+  name              = "/aws/lambda/${aws_lambda_function.function.function_name}"
+  retention_in_days = var.cloudwatch_log_retention_in_days
 }
