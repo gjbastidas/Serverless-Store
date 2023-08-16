7a51caf3ba4ad5865a9274e6f358d25d2bf2baa3
gustavo bastidas <gjbastidas@gmail.com>
2023-08-15 21:02:22 -0600

    added createoneproduct return error

--- backend/store_apis/pkg/products/products_test.go
+++ backend/store_apis/pkg/products/products_test.go
@@ -2,6 +2,7 @@ package products
 
 import (
 	"context"
+	"errors"
 	"net/http"
 	"os"
 	"store_apis/pkg/config"
@@ -17,12 +18,10 @@ import (
 )
 
 func Test_CreateOneProduct_ReturnOK(t *testing.T) {
-	ctx := context.TODO()
-
 	body := `
 	{
-		"name": "one product",
-  	"description": "one product description"
+		"name": "valid product",
+  	"description": "valid product description"
 	}
 	`
 
@@ -54,9 +53,100 @@ func Test_CreateOneProduct_ReturnOK(t *testing.T) {
 	}
 
 	p := new(Product)
-	resp, err := p.createOneProduct(ctx, req, cfg, awsSvc)
+
+	resp, err := p.createOneProduct(context.TODO(), req, cfg, awsSvc)
 	assert.NoError(t, err)
 
 	assert.Equal(t, http.StatusCreated, resp.StatusCode)
 	assert.Contains(t, resp.Body, "successfully created product with id:")
+}
+
+func Test_CreateOneProduct_ReturnError(t *testing.T) {
+	subtests := []struct {
+		name          string
+		body          string
+		expected      int
+		expectedError string
+	}{
+		{
+			name: "invalid_product_name",
+			body: `
+				{
+					"name": "",
+					"description": "invalid product description"
+				}
+			`,
+			expected:      http.StatusBadRequest,
+			expectedError: "error product validation",
+		},
+		{
+			name: "invalid_product_description",
+			body: `
+				{
+					"name": "invalid product",
+					"description": ""
+				}
+			`,
+			expected:      http.StatusBadRequest,
+			expectedError: "error product validation",
+		},
+		{
+			name: "error_putting_item",
+			body: `
+				{
+					"name": "valid product name",
+					"description": "valid product description"
+				}
+			`,
+			expected:      http.StatusInternalServerError,
+			expectedError: "error putting item",
+		},
+	}
+
+	cfg := new(config.Cfg)
+	os.Setenv("PRODUCTS_TABLE", "test")
+	err := envconfig.Process("", cfg)
+	assert.NoError(t, err)
+
+	for _, st := range subtests {
+		t.Run(st.name, func(t *testing.T) {
+			req := events.APIGatewayProxyRequest{
+				Headers:    map[string]string{"content-type": "application/json"},
+				Resource:   "/products",
+				Path:       "/products",
+				HTTPMethod: http.MethodPost,
+				Body:       st.body,
+			}
+
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			mockDdbClient := mock_aws_services.NewMockDynamoDBClientAPI(ctrl)
+
+			switch st.expectedError {
+			case "error putting item":
+				mockDdbClient.
+					EXPECT().
+					PutItem(gomock.Any(), gomock.Any()).
+					Return(nil, errors.New("error putting item: some detailed error"))
+			default:
+				mockDdbClient.
+					EXPECT().
+					PutItem(gomock.Any(), gomock.Any()).
+					Return(nil, nil).
+					AnyTimes() // expects zero or more calls
+			}
+
+			awsSvc := &aws_services.AWS{
+				DDBClient: mockDdbClient,
+			}
+
+			p := new(Product)
+			resp, err := p.createOneProduct(context.TODO(), req, cfg, awsSvc)
+			assert.NoError(t, err)
+
+			assert.Equal(t, st.expected, resp.StatusCode)
+			assert.Contains(t, resp.Body, st.expectedError)
+		})
+	}
 }
